9b2671cdc82cacb77f69d79fff0da864be7098c6
tanmayidev <tanmayi.d.99@gmail.com>
2022-11-20 14:15:46 +0530

    Add test case to check URL
    add function for parseURL and test case for querying URL

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,5 +1,10 @@
+import { UrlWithParsedQuery, parse } from "url";
 
 export class Utils {
+
+    public static parseUrl(url: string): UrlWithParsedQuery{
+        return parse(url, true);
+    }
 
     public static toUpperCase(arg: string): string {
         return arg.toUpperCase();
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,3 +1,4 @@
+import { parse } from 'path';
 import { Utils } from '../app/Utils';
 
 describe('Utils test suite', () => {
@@ -6,6 +7,26 @@ describe('Utils test suite', () => {
         const result = Utils.toUpperCase('abc');
         //assertion
         expect(result).toBe('ABC');
+    });
+
+    test('parse simple URL', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parsedUrl.href).toBe('http://localhost:8080/login');
+        expect(parsedUrl.port).toBe('8080');
+        expect(parsedUrl.protocol).toBe('http:');
+        // if .toBe() used, we get  "If it should pass with deep equality, replace 'toBe' with 'toEqual' "
+        // toEqual is used for objects (for primitives we use toBe)
+        expect(parsedUrl.query).toEqual({});
+    });
+
+    test('parse URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+        const expectedQuery = {
+            user: 'user',
+            password: 'pass'
+        }
+        expect(parsedUrl.query).toEqual(expectedQuery);      
+        // expect(expectedQuery).toBe(expectedQuery);     
     });
 
 });
